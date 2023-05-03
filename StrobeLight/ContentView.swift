c8ba7279b17910a044dc5e5447f428d8d9089a2c
Jesse Born <jesse.born@gmx.ch>
2023-05-03 10:20:40 +0200

    Initial Commit

---
+++ StrobeLight/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StrobeLight
+//
+//  Created by Jesse Born on 03.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StrobeLight/StrobeLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrobeLightApp.swift
+//  StrobeLight
+//
+//  Created by Jesse Born on 03.05.23.
+//
+
+import SwiftUI
+
+@main
+struct StrobeLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
