f6b50efbcd4372bc8112370dd148ec8c7540c1a9
Jesse Born <jesse.born@gmx.ch>
2023-05-12 23:38:07 +0200

    new better html

--- makeDocs.sh
+++ makeDocs.sh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 
-swift package \
-    --allow-writing-to-directory ./docs \
-    generate-documentation \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path StrobeLight \
-    --output-path ./docs
-
+xcodebuild -project StrobeLight.xcodeproj -derivedDataPath docsData -scheme StrobeLight -destination 'platform=iOS Simulator,name=iPhone 14 Pro' -parallelizeTargets docbuild
+mkdir doc_archives
+cp -R `find docsData -type d -name "*.doccarchive"` doc_archives
+$(xcrun --find docc) process-archive transform-for-static-hosting ./doc_archives/StrobeLight.doccarchive --hosting-base-path StrobeLight/ --output-path docs/$ARCHIVE_NAME
