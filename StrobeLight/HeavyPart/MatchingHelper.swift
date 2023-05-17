ade932fca77892915a64fe63a9cd787d340d9210
Jesse Born <jesse.born@gmx.ch>
2023-05-17 11:12:08 +0200

    first draft of shazam integration

---
+++ StrobeLight/HeavyPart/MatchingHelper.swift
@@ -0,0 +1,8 @@
+//
+//  MatchingHelper.swift
+//  StrobeLight
+//
+//  Created by Jesse Born on 15.05.23.
+//
+
+import Foundation
---
+++ StrobeLight/Views/ShazamSongView.swift
@@ -0,0 +1,20 @@
+//
+//  ShazamSongView.swift
+//  StrobeLight
+//
+//  Created by Jesse Born on 15.05.23.
+//
+
+import SwiftUI
+
+struct ShazamSongView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShazamSongView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShazamSongView()
+    }
+}
